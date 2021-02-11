f0568f30a8df6b1d27c53497a3603d0ee117a97b
xhandram <belladamix@gmail.com>
2021-02-11 09:50:50 +0100

    Update settings.py

--- kestrel/settings.py
+++ kestrel/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '*u6%yez5)9jdk5h%n1re4v!f4fgu)mpsosaimc99*k*(a)^$6+'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
