8d520965ad96fd2cac75ee7316c95bf75d17bd93
Belladamix <belladamix@gmail.com>
2021-02-15 14:21:54 +0100

    Refactoring

---
+++ core/courier/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth import login
+from django.contrib.auth.decorators import login_required
+
+
+# Create your views here.
+
+
+@login_required()
+def home(request):
+    return render(request,'home.html')
+
--- core/views.py
+++ core/views.py
@@ -1,18 +1,9 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import login
-from django.contrib.auth.decorators import login_required
 from . import forms 
 
 # Create your views here.
 def home(request):
-    return render(request,'home.html')
-
-@login_required()
-def customer(request):
-    return render(request,'home.html')
-
-@login_required()
-def courier(request):
     return render(request,'home.html')
 
 def sign_up(request):
